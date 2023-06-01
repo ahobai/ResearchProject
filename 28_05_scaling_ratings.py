2e95dab410a140cdc3272bdd81d379ccc480a916
AnaHobai <a.hobai@student.tudelft.nl>
2023-06-01 19:53:07 +0200

    Created the target data set by dealing with the overlapped segments between two annotators in the involvement annnotations (if the anotations were the same, the value was kept, otherwise a weighted average would be computed).

--- 28_05_scaling_ratings.py
+++ 28_05_scaling_ratings.py
@@ -36,26 +36,7 @@ annotator4 = df_ratings[df_ratings['Annotator'] == 4]
 # 
 # r4q1 = annotator4[annotator4['Group'] == 2]
 # r4q1 = r4q1['Involvement']
-# avg4 = sum(r4q1)/len(r4q1)
-# 
-# 
-# #average for group 3 for each annotator
-# 
-# r1q1 = annotator1[annotator1['Group'] == 3]
-# r1q1 = r1q1['Involvement']
-# avg1 = sum(r1q1)/len(r1q1)
-# 
-# r2q1 = annotator2[annotator2['Group'] == 3]
-# r2q1 = r2q1['Involvement']
-# avg2 = sum(r2q1)/len(r2q1)
-# 
-# r3q1 = annotator3[annotator3['Group'] == 3]
-# r3q1 = r3q1['Involvement']
-# avg3 = sum(r3q1)/len(r3q1)
-# 
-# r4q1 = annotator4[annotator4['Group'] == 3]
-# r4q1 = r4q1['Involvement']
-# avg4 = sum(r4q1)/len(r4q1)
+# avg4 = sum(r4q1)/len(r4q1)=
 # =============================================================================
 
 # involvement per annotator for each group
@@ -86,7 +67,9 @@ for x in range(2, 16):
     r4.append(avg4)
 
 
-
+# =============================================================================
+# COHEN KAPPA SCORE
+# =============================================================================
 
 from sklearn.metrics import cohen_kappa_score
 # get the overlap between each two annotators
@@ -117,7 +100,10 @@ kappa_r2r3 = cohen_kappa_score(r2, r3)
 kappa_r2r4 = cohen_kappa_score(r2, r4)
 kappa_r3r4 = cohen_kappa_score(r3, r4)
 
-raters = pd.DataFrame({'r1': r1, 'r4': r4, 'r2': r2, 'r3': r3})
+
+# =============================================================================
+# OBSERVED AGREEMENT
+# =============================================================================
 
 # Calculate observed agreement
 total_items = len(r2)
@@ -162,30 +148,69 @@ agreement_count = sum(1 for i in range(total_items) if r3[i] == r4[i])
 observed_agreement34_avg = agreement_count / total_items
 
 
+# =============================================================================
+# ICC INTER-ANNOTATOR AGREEMENT
+# =============================================================================
+
+df_ratings['Group_session'] = "g" + df_ratings['Group'].astype(str) +"-s"+ df_ratings['Session'].astype(str)
+data = df_ratings[['Group_session', 'Annotator', 'Involvement']]
+data12 = data.loc[data['Annotator'].isin([1, 2])]
+data12 = data12.loc[data12.duplicated()]
+data13 = data.loc[data['Annotator'].isin([1, 3])]
+data13 = data13.loc[data13.duplicated()]
+data14 = data.loc[data['Annotator'].isin([1, 4])]
+data14 = data14.loc[data14.duplicated()]
+data23 = data.loc[data['Annotator'].isin([2, 3])]
+data23 = data23.loc[data23.duplicated()]
+data24 = data.loc[data['Annotator'].isin([2, 4])]
+data24 = data24.loc[data24.duplicated()]
+data34 = data.loc[data['Annotator'].isin([3, 4])]
+data34 = data34.loc[data34.duplicated()]
+
+import pingouin as pg
+
+# Calculate ICC
+icc_result12 = pg.intraclass_corr(data=data12, targets='Group_session', raters='Annotator', ratings='Involvement')
+icc_result13 = pg.intraclass_corr(data=data13, targets='Group_session', raters='Annotator', ratings='Involvement')
+icc_result14 = pg.intraclass_corr(data=data14, targets='Group_session', raters='Annotator', ratings='Involvement')
+icc_result23 = pg.intraclass_corr(data=data23, targets='Group_session', raters='Annotator', ratings='Involvement')
+icc_result24 = pg.intraclass_corr(data=data24, targets='Group_session', raters='Annotator', ratings='Involvement')
+icc_result34 = pg.intraclass_corr(data=data34, targets='Group_session', raters='Annotator', ratings='Involvement')
+
+
+
+# =============================================================================
+# Build the dataset, dealing with the overlapped segments
+# annotated by different annotators by either keeping that value if it is equal
+# or keeping the weighted mean (weights = annotators) otherwise
+# =============================================================================
+
+# Group the dataframe by the columns that contain duplicate values
+df = df_ratings[['Start Time (ms)', 'End Time (ms)', 'Group', 'Session', 'Annotator', 'Involvement']] # has 3247 rows
+groups = df.groupby(['Start Time (ms)', 'End Time (ms)', 'Group', 'Session']) # are 1599 duplicates
+duplicates = []
+
+# Iterate over each group
+for (start, end, group, session), annotations in groups:
+    if len(annotations) > 1:  # Check if there are duplicate rows
+        duplicates.append(annotations)
+        
+        if not annotations.empty:
+            # Check if the duplicated rows are not equal
+            if not annotations.equals(annotations.iloc[0]):
+                # Calculate the weighted mean
+                weighted_mean = (annotations['Involvement'] * annotations['Annotator']).sum() / annotations['Annotator'].sum()
+                
+                # Update all duplicated rows with the weighted mean
+                df.loc[annotations.index, 'Involvement'] = weighted_mean
+            
+
+final_df = df.drop_duplicates(subset=df.columns.difference(['Annotator'])) # has 1599 rows, ????so what happens to the extra 49 rows
 
 
 
 
 
 
-
-
-
-
-
-
-
-df_ratings = df_ratings.groupby(['Start Time (ms)', 'End Time (ms)']) ## combine start time and end time
-
-
-
-#Step 1
-df_average_rating = df_ratings.groupby(['Group', 'Session']).mean('Involvement')
-
-#Step 2
-df_factors1 = df_ratings.loc[df_ratings['Annotator'] == 1]/df_average_rating
-df_factors2 = df_ratings.loc[df_ratings['Annotator'] == 2]/df_average_rating
-df_factors3 = df_ratings.loc[df_ratings['Annotator'] == 3]/df_average_rating
-df_factors4 = df_ratings.loc[df_ratings['Annotator'] == 4]/df_average_rating
 
 
